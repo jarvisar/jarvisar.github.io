e26adc5c768832fa13f8f5a53801feb9686603be
jarvisar <jarvisar@mail.uc.edu>
2023-04-27 23:46:43 -0400

    Added wave background

---
+++ assets/js/hero.js
@@ -0,0 +1,67 @@
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.149.0/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from "https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js"
+import * as THREE from "https://cdn.skypack.dev/three@0.88.0";
+
+// load computer glb using threejs, and put in div id=3d 
+var scene = new THREE.Scene();
+
+const canvas = document.querySelector('.hero-canvas');
+var renderer = new THREE.WebGLRenderer({ canvas, alpha: true, antialias: true });
+renderer.setClearColor(0x333333, 1);
+// get width of canvas
+var divWidth = canvas.clientWidth;
+var divHeight = canvas.clientHeight;
+renderer.setSize( divWidth, divHeight );
+
+var vertexHeight = 15000,
+    planeDefinition = 100,
+    planeSize = 1245000;
+
+var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 400000)
+camera.position.z = 10000;
+camera.position.y = 50000;
+
+// look at a downward angle to the right
+camera.rotation.x = -0.61;
+
+var planeGeo = new THREE.PlaneGeometry(planeSize, planeSize, planeDefinition, planeDefinition);
+var plane = new THREE.Mesh(planeGeo, new THREE.MeshBasicMaterial({
+    color: 0xbbbbbb,
+    wireframe: true
+}));
+plane.rotation.x -= Math.PI * .5;
+
+scene.add(plane);
+
+updatePlane();
+
+function updatePlane() {
+    console.log(planeGeo)
+    for (var i = 0; i < planeGeo.vertices.length; i++) {
+        planeGeo.vertices[i].z += Math.random() * vertexHeight * 1.5 - vertexHeight;
+        planeGeo.vertices[i]._myZ = planeGeo.vertices[i].z
+    }
+};
+
+render();
+
+var count = 0
+function render() {
+    requestAnimationFrame(render);
+    // camera.position.z -= 150;
+    var x = camera.position.x;
+    var z = camera.position.z;
+
+    for (var i = 0; i < planeGeo.vertices.length; i++) {
+        var z = +planeGeo.vertices[i].z;
+        var randomOffset = (Math.random() - 0.5) * 10; // generates a random number between -5 and 5
+        planeGeo.vertices[i].z = Math.sin(( i + count * 0.000002)) * (planeGeo.vertices[i]._myZ - (planeGeo.vertices[i]._myZ* 0.4)) + randomOffset;
+        plane.geometry.verticesNeedUpdate = true;
+    
+        count += 0.1
+    }
+    
+
+    renderer.render(scene, camera);
+}
+
