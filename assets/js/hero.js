0315ad30f6471887afcdeead912eda7525174607
jarvisar <jarvisar@mail.uc.edu>
2023-04-28 00:55:01 -0400

    Added more planes

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -34,9 +34,34 @@ var plane = new THREE.Mesh(planeGeo, new THREE.MeshBasicMaterial({
 }));
 plane.rotation.x -= Math.PI * .5;
 
+var plane2Geo = new THREE.PlaneGeometry(planeSize, planeSize, planeDefinition, planeDefinition);
+var plane2 = new THREE.Mesh(plane2Geo, new THREE.MeshBasicMaterial({
+    color: 0x333333,
+    wireframe: true,
+    wireframeLinewidth: 1 
+}));
+plane2.position.y = -10000; 
+plane2.position.x = 7000;
+plane2.rotation.x -= Math.PI * .5;
+
+var plane3Geo = new THREE.PlaneGeometry(planeSize, planeSize, planeDefinition, planeDefinition);
+var plane3 = new THREE.Mesh(plane3Geo, new THREE.MeshBasicMaterial({
+    color: 0x666666,
+    wireframe: true,
+    wireframeLinewidth: 1
+}));
+plane3.position.y = -20000;
+plane3.position.x = -10000;
+plane3.rotation.x -= Math.PI * .5;
+
 scene.add(plane);
+scene.add(plane2);
+scene.add(plane3);
 
 updatePlane();
+updatePlane2();
+updatePlane3();
+
 
 function updatePlane() {
     console.log(planeGeo)
@@ -46,18 +71,54 @@ function updatePlane() {
     }
 };
 
+function updatePlane2() {
+    for (var i = 0; i < plane2Geo.vertices.length; i++) {
+        plane2Geo.vertices[i].z += Math.random() * vertexHeight * 1.5 - vertexHeight;
+        plane2Geo.vertices[i]._myZ = plane2Geo.vertices[i].z
+    }
+};
+
+function updatePlane3() {
+    for (var i = 0; i < plane3Geo.vertices.length; i++) {
+        plane3Geo.vertices[i].z += Math.random() * vertexHeight * 1.5 - vertexHeight;
+        plane3Geo.vertices[i]._myZ = plane3Geo.vertices[i].z
+    }
+};
+
+
 render();
 
 var count = 0
 function render() {
     requestAnimationFrame(render);
     
+    // update vertices of first plane
     for (var i = 0; i < planeGeo.vertices.length; i++) {
         var z = +planeGeo.vertices[i].z;
-        var randomOffset = (Math.random() - 0.5) * 10; // generates a random number between -5 and 5
+        var randomOffset = (Math.random() - 0.5) * 10;
         planeGeo.vertices[i].z = Math.sin(( i + count * 0.000002)) * (planeGeo.vertices[i]._myZ - (planeGeo.vertices[i]._myZ* 0.4)) + randomOffset;
         plane.geometry.verticesNeedUpdate = true;
     
+        count += 0.1
+    }
+
+    // update vertices of second plane
+    for (var i = 0; i < plane2Geo.vertices.length; i++) {
+        var z = +plane2Geo.vertices[i].z;
+        var randomOffset = (Math.random() - 0.5) * 10;
+        plane2Geo.vertices[i].z = Math.cos(( i + count * 0.000002)) * (plane2Geo.vertices[i]._myZ - (plane2Geo.vertices[i]._myZ* 0.5)) + randomOffset;
+        plane2.geometry.verticesNeedUpdate = true;
+    
+        count += 0.1
+    }
+
+    // update vertices of third plane
+    for (var i = 0; i < plane3Geo.vertices.length; i++) {
+        var z = +plane3Geo.vertices[i].z;
+        var randomOffset = (Math.random() - 0.5) * 10;
+        plane3Geo.vertices[i].z = Math.sin(( i + count * 0.000002)) * (plane3Geo.vertices[i]._myZ - (plane3Geo.vertices[i]._myZ* 0.6)) + randomOffset;
+        plane3.geometry.verticesNeedUpdate = true;
+
         count += 0.1
     }
 
